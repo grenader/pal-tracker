9ffaf382cf1b390a085f97bd0f508ad61436604f
grenader <igor.kanshyn@gmail.com>
2020-07-27 13:09:24 -0400

    added different unit/integration tests

---
+++ src/test/java/io/pivotal/pal/tracker/PalTrackerApplicationTest.java
@@ -0,0 +1,15 @@
+package io.pivotal.pal.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class PalTrackerApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ src/test/java/io/pivotal/pal/tracker/SmokeTest.java
@@ -0,0 +1,20 @@
+package io.pivotal.pal.tracker;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class SmokeTest {
+
+    @Autowired
+    private WelcomeController controller;
+
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+}
