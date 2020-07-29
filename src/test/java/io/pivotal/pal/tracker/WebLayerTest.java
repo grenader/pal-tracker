3541aa33b0dcb6316aca1e1c8978d3756f2c7696
grenader <igor.kanshyn@gmail.com>
2020-07-29 12:22:50 -0400

    fixed the unit tests

--- src/test/java/io/pivotal/pal/tracker/WebLayerTest.java
+++ src/test/java/io/pivotal/pal/tracker/WebLayerTest.java
@@ -20,6 +20,6 @@ public class WebLayerTest {
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("hello")));
+                .andExpect(content().string(containsString("Hello from test")));
     }
 }
